49fc563c86b68f09ca072b33b18a9fe2e9c5020f
casakc <138028480+casakc@users.noreply.github.com>
2025-01-29 16:26:29 +0100

    Initial React Stu«ruture updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo1.jpg';
 import './App.css';
 
 export default function App() {
@@ -6,7 +6,8 @@ export default function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid" alt="logo" />
-       Hello
+       <a href="" className="btn btn-primary">
+       Hello</a>
       </header>
     </div>
   );
