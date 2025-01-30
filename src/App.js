e2051a3e156608361b747ffb30a88e234fdfd6c0
casakc <138028480+casakc@users.noreply.github.com>
2025-01-30 19:21:37 +0100

    "Dictionary" Component (.js + .css) created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import logo from './logo1.jpg';
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
+      <div className='container'>
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid" alt="logo" />
        Hello
       </header>
+      <main>
+        <Dictionary />
+        </main>
+      <footer className='App-footer'><small>Coded by Catarina S-A</small></footer>
+    </div>
     </div>
   );
 }
